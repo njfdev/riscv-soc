d513ba4eb049e3cb9c256506235cf64731e20c86
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2026-07-12 12:08:24 -0400

    feat: riscv hello world assembly

---
+++ programs/hello_world.s
@@ -0,0 +1,15 @@
+.global _start
+
+.text
+_start:
+  li  a0,1        # stdout = 1
+  la  a1,message  # buff ptr
+  li  a2,12       # buff length
+  li  a7,64       # code for write
+  ecall
+
+  li a0, 42       # exit status
+  li a7, 93       # Linux exit syscall
+  ecall
+
+message: .ascii "Hello World\n"
