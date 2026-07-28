33627254e2615d4198d3c4010c0e0ce2b844e324
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2026-07-27 21:32:45 -0400

    feat: add JAL functionality

--- rtl/cpu/cpu_top.sv
+++ rtl/cpu/cpu_top.sv
@@ -25,12 +25,19 @@ module cpu_top #(
   wire isALUImm = instr[6:2] == 5'b00100;
   wire isALUReg = instr[6:2] == 5'b01100;
   wire isSystem = instr[6:2] == 5'b11100;
+
+  wire [11:0] immI = instr[31:20];
+  wire [11:0] immS = {instr[31:25], instr[11:7]};
+  wire [11:0] immB = {instr[31], instr[7], instr[30:25], instr[11:8]}; // remember, this actually represents bits 12:1
+  wire [19:0] immU = instr[31:12];
+  wire [19:0] immJ = {instr[31], instr[19:12], instr[20], instr[30:21]}; // remember, this actually represents bits 20:1
+
   
   /************************ ALU ************************/
 
   
 
-  logic [31:0] regFile [0:31] = '{default:'0};
+  logic [31:0] regFile [0:31] /* verilator public_flat_rw */ = '{default:'0};
 
   // doesn't include system, store, or branch instructions
   wire [4:0] rdIndex = instr[11:7];
@@ -46,9 +53,9 @@ module cpu_top #(
 
   logic [ADDR_WIDTH-1:0] pc = (ADDR_WIDTH)'(RESET_ADDR);
 
-  wire [ADDR_WIDTH-1:0] nextPc = pc + 4;
+  wire [ADDR_WIDTH-1:0] jalOffset = $signed({{(ADDR_WIDTH-21){immJ[19]}}, immJ[19:0], 1'b0});
 
-
+  wire [ADDR_WIDTH-1:0] nextPc = isJAL ? pc + jalOffset : pc + 4;
 
   /******************* Decode Logic ********************/
 
@@ -71,6 +78,19 @@ module cpu_top #(
       end
 
       EXECUTE_STATE: begin
+
+        // update rd if necessary
+        if (rdIndex != 0) begin
+          case (1)
+
+            isJAL: begin
+              /* verilator lint_off WIDTHEXPAND */
+              regFile[rdIndex] = pc+4;
+              /* verilator lint_on WIDTHEXPAND */
+            end
+
+          endcase
+        end
 
         pc <= nextPc;
         state <= FETCH_STATE;
--- tests/verilator/main.cpp
+++ tests/verilator/main.cpp
@@ -13,13 +13,20 @@ void cycle(std::unique_ptr<Vsoc>& soc) {
   soc->eval();
 }
 
+void print_regs(std::unique_ptr<Vsoc>& soc) {
+  printf("Regs:\n");
+  for (int i = 0; i < 32; i++) {
+    printf("\t%ix: %08x\n", i, soc->rootp->soc__DOT__cpu0__DOT__regFile[i]);
+  }
+}
+
 int main() {
   auto soc = std::make_unique<Vsoc>();
 
   // basic assembly program
   soc->rootp->soc__DOT__ram0__DOT__ram[0] = 0b000000010010'00010'000'00011'0010011;   // ADDI x3, x2, 17
   soc->rootp->soc__DOT__ram0__DOT__ram[1] = 0b0000000'00011'00010'000'00001'0110011;  // ADD x1, x2, x3
-  soc->rootp->soc__DOT__ram0__DOT__ram[2] = 0b00000000000000000000'00000'1101111;     // JAL x0, 0
+  soc->rootp->soc__DOT__ram0__DOT__ram[2] = 0b11111111100111111111'00001'1101111;     // JAL x1, -8
 
   do {
     cycle(soc);
@@ -27,6 +34,7 @@ int main() {
     printf("\tData at PC: %08x\n", soc->rootp->soc__DOT__ram0__DOT__ram[soc->rootp->soc__DOT__cpu0__DOT__pc/4]);
     printf("\tInstr register: %08x\n", soc->rootp->soc__DOT__cpu0__DOT__instr);
     printf("Phase: %s\n", soc->rootp->soc__DOT__cpu0__DOT__state == 1 ? "execute" : "fetch");
+    print_regs(soc);
   } while (std::cin.get() != 'q');
 
   soc->final();
